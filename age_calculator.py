c0bec8bcba577cc274f6c79e53889aaabb5d9cf2
darkangel87896-spec <darkangel87896@gmail.com>
2026-03-01 17:18:19 +0500

    Create age_calculator.py

---
+++ age_calculator.py
@@ -0,0 +1,7 @@
+name = input("Ismingizni kiriting: ")
+birth_year = int(input("Tug‘ilgan yilingizni kiriting: "))
+
+current_year = 2026
+age = current_year - birth_year
+
+print(name, "siz", age, "yoshdasiz.")
